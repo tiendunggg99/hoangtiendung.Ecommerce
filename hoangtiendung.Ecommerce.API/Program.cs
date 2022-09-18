81a94afab90f63d4274722fea80ed6c8304fa024
hoangtiendung <tiendung.gg99@gmail.com>
2022-09-18 22:25:33 +0700

    regist DI

--- hoangtiendung.Ecommerce.API/Program.cs
+++ hoangtiendung.Ecommerce.API/Program.cs
@@ -4,6 +4,7 @@ using hoangtiendung.Ecommerce.API.ServiceCollection;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddServiceRegisters(builder.Configuration);
+builder.Services.AddDIRegisters();
 
 var app = builder.Build();
 
