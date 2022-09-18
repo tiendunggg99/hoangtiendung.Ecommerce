b7a71587fc9fe6bfee616465306b4167dae1b70f
hoangtiendung <tiendung.gg99@gmail.com>
2022-09-18 22:28:44 +0700

    regist Mapper

---
+++ hoangtiendung.Ecommerce.API/MapperProfile/MapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using hoangtiendung.Ecommerce.BLL.Models;
+using hoangtiendung.Ecommerce.DAL.Entities;
+
+namespace hoangtiendung.Ecommerce.API.MapperProfile
+{
+    public class MapperProfileL : Profile
+    {
+        public MapperProfileL()
+        {
+            CreateMap<CategoryModel, Category>().ReverseMap();
+            CreateMap<Order, OrderModel>().ReverseMap();
+            CreateMap<OrderDetail, OrderDetailModel>().ReverseMap();
+            CreateMap<Order, OrderModel>().ReverseMap();
+            CreateMap<Product, ProductModel>().ReverseMap();
+            CreateMap<Promotion, PromotionModel>().ReverseMap();
+            CreateMap<Photo, PhotoModel>().ReverseMap();
+        }
+    }
+}
